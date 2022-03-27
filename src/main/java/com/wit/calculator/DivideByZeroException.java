135c08fa2fdffe83b8e1d0ce242facb668382791
Luiz Carlos Fernandes <luizcig@gmail.com>
2022-03-27 08:20:51 -0300

    Add files via upload

---
+++ src/main/java/com/wit/calculator/DivideByZeroException.java
@@ -0,0 +1,12 @@
+package main.java.com.wit.calculator;
+
+public class DivideByZeroException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1458652593820900251L;
+
+	public DivideByZeroException(String message) {
+		super(message);
+	}
+}
